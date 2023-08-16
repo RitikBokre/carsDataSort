24451bc1c17d933d5bfef29b3fef300e6503d03e
Ritik Bokre <ritikbokre100@gmail.com>
2023-08-16 18:49:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,292 @@
+const variantData = [
+  {
+    verlabel: "VXI",
+    isDiscontinued: false,
+    verid: "5547",
+    verprice: "₹ 11.09 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 11, value: "Diesel" },
+      TransimissionType: { itemId: 2, customId: 21, value: "Automatic CVT" },
+      SeaterType: { itemId: 3, customId: 32, value: "5 Seater" },
+    },
+    onClick: null,
+  },
+  {
+    verlabel: "LDI",
+    isDiscontinued: false,
+    verid: "5547",
+    verprice: "₹ 12.5 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 13, value: "Petrol" },
+      TransimissionType: { itemId: 2, customId: 22, value: "Manual" },
+      SeaterType: { itemId: 3, customId: 33, value: "7 Seater" },
+    },
+    onClick: null,
+  },
+  {
+    verlabel: "VXI rms",
+    isDiscontinued: false,
+    verid: "5547",
+    verprice: "₹ 13.5 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 12, value: "CNG" },
+      TransimissionType: {
+        itemId: 2,
+        customId: 23,
+        value: "Automatic + Manual",
+      },
+      SeaterType: { itemId: 3, customId: 31, value: "3 Seater" },
+    },
+    onClick: null,
+  },
+  {
+    verlabel: "VXI df",
+    isDiscontinued: false,
+    verid: "5547",
+    verprice: "₹ 7.09 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 11, value: "Diesel" },
+      TransimissionType: {
+        itemId: 2,
+        customId: 23,
+        value: "Automatic + Manual",
+      },
+      SeaterType: { itemId: 3, customId: 31, value: "3 Seater" },
+    },
+    onClick: null,
+  },
+  {
+    verlabel: "VXI jhg",
+    verid: "5547",
+    verprice: "₹ 9 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 11, value: "Diesel" },
+      TransimissionType: { itemId: 2, customId: 22, value: "Manual" },
+      SeaterType: { itemId: 3, customId: 31, value: "3 Seater" },
+    },
+    onClick: null,
+  },
+  {
+    verlabel: "LDI f",
+    isDiscontinued: false,
+    verid: "5547",
+    verprice: "₹ 19.3 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 11, value: "Diesel" },
+      TransimissionType: { itemId: 2, customId: 22, value: "Manual" },
+      SeaterType: { itemId: 3, customId: 33, value: "7 Seater" },
+    },
+    onClick: null,
+  },
+  {
+    verlabel: "VXI sf",
+    isDiscontinued: false,
+    verid: "5547",
+    verprice: "₹ 16.2 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 11, value: "Diesel" },
+      TransimissionType: { itemId: 2, customId: 22, value: "Manual" },
+      SeaterType: { itemId: 3, customId: 34, value: "8 Seater" },
+    },
+    onClick: null,
+  },
+  {
+    verlabel: "VXI sdf",
+    isDiscontinued: false,
+    verid: "5547",
+    verprice: "₹ 12.4 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 12, value: "CNG" },
+      TransimissionType: {
+        itemId: 2,
+        customId: 23,
+        value: "Automatic + Manual",
+      },
+      SeaterType: { itemId: 3, customId: 31, value: "3 Seater" },
+    },
+    onClick: null,
+  },
+  {
+    verlabel: "LDI re",
+    isDiscontinued: true,
+    verid: "5547",
+    verprice: "₹ 18 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 11, value: "Diesel" },
+      TransimissionType: { itemId: 2, customId: 21, value: "Automatic CVT" },
+      SeaterType: { itemId: 3, customId: 32, value: "5 Seater" },
+    },
+    onClick: null,
+  },
+  {
+    verlabel: "VXI kfs",
+    isDiscontinued: true,
+    verid: "5547",
+    verprice: "₹ 25 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 12, value: "CNG" },
+      TransimissionType: {
+        itemId: 2,
+        customId: 23,
+        value: "Automatic + Manual",
+      },
+      SeaterType: { itemId: 3, customId: 31, value: "3 Seater" },
+    },
+    onClick: null,
+  },
+  {
+    verlabel: "LDI pov",
+    isDiscontinued: true,
+    verid: "5547",
+    verprice: "₹ 45 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 11, value: "Diesel" },
+      TransimissionType: { itemId: 2, customId: 22, value: "Manual" },
+      SeaterType: { itemId: 3, customId: 34, value: "8 Seater" },
+    },
+    onClick: null,
+  },
+  {
+    verlabel: "VXI dfbhv",
+    isDiscontinued: true,
+    verid: "5547",
+    verprice: "₹ 7 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 12, value: "CNG" },
+      TransimissionType: { itemId: 2, customId: 22, value: "Manual" },
+      SeaterType: { itemId: 3, customId: 34, value: "8 Seater" },
+    },
+    onClick: null,
+  },
+  {
+    verlabel: "LDI obs",
+    isDiscontinued: true,
+    verid: "5547",
+    verprice: "₹ 15.5 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 11, value: "Diesel" },
+      TransimissionType: { itemId: 2, customId: 21, value: "Automatic CVT" },
+      SeaterType: { itemId: 3, customId: 32, value: "5 Seater" },
+    },
+    onClick: null,
+  },
+  {
+    verlabel: "VXI dgfd",
+    isDiscontinued: true,
+    verid: "5547",
+    verprice: "₹ 21 Lakhs",
+    verinfo: {
+      fuelType: { itemId: 1, customId: 12, value: "CNG" },
+      TransimissionType: { itemId: 2, customId: 22, value: "Manual" },
+      SeaterType: { itemId: 3, customId: 31, value: "3 Seater" },
+    },
+    onClick: null,
+  },
+];
+
+// var array = [
+//   { itemId: 1, customId: 11, value: "Diesel", on: false },
+//   { itemId: 1, customId: 12, value: "CNG", on: false },
+//   { itemId: 1, customId: 13, value: "Petrol", on: false },
+//   { itemId: 2, customId: 21, value: "Automatic CVT", on: false },
+//   { itemId: 2, customId: 22, value: "Manual", on: false },
+//   { itemId: 2, customId: 23, value: "Automatic + Manual", on: false },
+//   { itemId: 3, customId: 31, value: "3 Seater", on: false },
+//   { itemId: 3, customId: 32, value: "5 Seater", on: false },
+//   { itemId: 3, customId: 33, value: "7 Seater", on: false },
+//   { itemId: 3, customId: 34, value: "8 Seater", on: false },
+// ];
+
+var array = [];
+
+for (let item of variantData) {
+  for (let key in item.verinfo) {
+    let isPush = true;
+    for (let i=0 ;i<array.length;i++) {
+      if (array[i]?.customId == item.verinfo[key].customId) {
+        isPush = false;
+      }
+    }
+    if (isPush) {
+      array.push(item.verinfo[key]);
+    }
+  }
+}
+
+var carList = document.querySelector(".car-list");
+var btnContainer = document.querySelector(".btn-container");
+
+function btnStat(type, subType) {
+  array = array.map((item) =>
+    item.customId == subType ? { ...item, on: !item.on } : item
+  );
+  // const idx = (type - 1) * 3 + (subType % 10);
+  carList.innerHTML = "";
+  renderList();
+  console.log(array);
+}
+
+function renderList() {
+  for (let i = 0; i < variantData.length; i++) {
+    const item = variantData[i];
+    const { fuelType, TransimissionType, SeaterType } = item.verinfo;
+    // const show = array.filter(
+    //   (carValue) =>
+    //     carValue.on &&
+    //     (carValue.customId === item.verinfo.fuelType.customId ||
+    //       carValue.customId === item.verinfo.TransimissionType.customId ||
+    //       carValue.customId === item.verinfo.SeaterType.customId)
+    // );
+
+    const show = array.filter((item) => item.on);
+    const showFuel = show.filter((item) => item.itemId == 1);
+    const showTrans = show.filter((item) => item.itemId == 2);
+    const showSeat = show.filter((item) => item.itemId == 3);
+    const show1 = showFuel.filter((item) => item.customId == fuelType.customId);
+    const show2 = showTrans.filter(
+      (item) => item.customId == TransimissionType.customId
+    );
+    const show3 = showSeat.filter(
+      (item) => item.customId == SeaterType.customId
+    );
+    let bool = true;
+    // if (show.length == 0) {
+    //   bool = false;
+    // }
+    if (showFuel.length != 0 && show1.length == 0) {
+      bool = false;
+    }
+    if (showTrans.length != 0 && show2.length == 0) {
+      bool = false;
+    }
+    if (showSeat.length != 0 && show3.length == 0) {
+      bool = false;
+    }
+
+    let car = `<ul class="list-element">
+                  <li>${item.verlabel}</li>
+                  <li>${item.verprice}</li>
+                  <li>
+                    <span>${item.verinfo.fuelType.value}</span>
+                    <span>${item.verinfo.TransimissionType.value}</span>
+                    <span>${item.verinfo.SeaterType.value}</span>
+                  </li>
+              </ul>`;
+    // if (show[0]) {
+    if (bool) {
+      carList.innerHTML += car;
+    }
+  }
+}
+
+renderList();
+
+for (let i = 0; i < array.length; i++) {
+  const itemNew = array[i];
+  let checkbox = `
+  <div>
+    <input type="checkbox" id="${itemNew.customId}" name="buttons" value="${itemNew.value}" onclick={btnStat(${itemNew.itemId},${itemNew.customId})}>
+    <label for="${itemNew.value}">${itemNew.value}</label>
+  <div>`;
+  btnContainer.innerHTML += checkbox;
+}
